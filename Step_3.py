cb838d28b61e9ba9effb58e60634244183be0a82
DrDonDon <53420422+DrDonDon@users.noreply.github.com>
2020-03-20 09:08:53 +1000

    Code to run step 3

--- Step_3.py
+++ Step_3.py
@@ -16,4 +16,6 @@ configuration = Configuration()
 configuration.host = "https://app.amphoradata.com"
 auth_api = a10a.AuthenticationApi(a10a.ApiClient(configuration))
 token_request = a10a.TokenRequest(username=os.getenv('username'), password=os.getenv('password')) 
+res = auth_api.authentication_request_token(token_request = token_request )
+configuration.api_key["Authorization"] = "Bearer " + res
 amphora_api = a10a.AmphoraeApi(a10a.ApiClient(configuration))
