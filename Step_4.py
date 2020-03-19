54d873d1976c5b39aa45dc737a9c4275577fe19a
DrDonDon <53420422+DrDonDon@users.noreply.github.com>
2020-03-19 16:42:35 +1000

    Fill out of basic code

--- Step_4.py
+++ Step_4.py
@@ -1,1 +1,29 @@
+# Import Amphora librarys
+import amphora_client as a10a
+from amphora_client.configuration import Configuration
+from amphora_extensions.file_uploader import FileUploader
 
+# Import non-Amphora librarys
+from array import array 
+import os
+import numpy as np
+import time
+from datetime import datetime, timedelta
+
+
+# Login to amphoradata.com
+configuration = Configuration()
+configuration.host = "https://app.amphoradata.com"
+auth_api = a10a.AuthenticationApi(a10a.ApiClient(configuration))
+token_request = a10a.TokenRequest(username=os.getenv('username'), password=os.getenv('password')) 
+amphora_api = a10a.AmphoraeApi(a10a.ApiClient(configuration))
+
+# Define model function
+
+# Define create Amphora function
+
+#####################################
+
+# Run model
+
+# Create Amphora
