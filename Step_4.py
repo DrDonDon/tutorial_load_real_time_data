755dd35285691a229dddcb743f5103b8d62a3a48
DrDonDon <53420422+DrDonDon@users.noreply.github.com>
2020-03-20 08:23:02 +1000

    Code to run Step 4

--- Step_4.py
+++ Step_4.py
@@ -15,7 +15,9 @@ from datetime import datetime, timedelta
 configuration = Configuration()
 configuration.host = "https://app.amphoradata.com"
 auth_api = a10a.AuthenticationApi(a10a.ApiClient(configuration))
-token_request = a10a.TokenRequest(username=os.getenv('username'), password=os.getenv('password')) 
+token_request = a10a.TokenRequest(username=os.getenv('username'), password=os.getenv('password'))
+res = auth_api.authentication_request_token(token_request = token_request )
+configuration.api_key["Authorization"] = "Bearer " + res
 amphora_api = a10a.AmphoraeApi(a10a.ApiClient(configuration))
 
 # Define model function
@@ -49,19 +51,19 @@ amphora_lon = 153.07106
 dto = a10a.CreateAmphora(name = amphora_name, lat = amphora_lat, lon = amphora_lon, 
                          price = amphora_price, description = amphora_description, 
                          terms_and_conditions_id = amphora_tnc, labels = amphora_labels)
-res = amphora_api.amphorae_create(create_amphora=dto)
+new_amphora = amphora_api.amphorae_create(create_amphora=dto)
 
 # Save Amphora ID for later
 File_object = open("Amphora_id.txt","w") 
-File_object.write(res.id)
+File_object.write(new_amphora.id)
 File_object.close()
 
 # Now create signal 
 yourSignal=a10a.Signal(_property = "timeProduct", value_type = "Numeric", 
                        attributes = {"units": "HMS"})
-amphora_api.amphorae_signals_create_signal(res.id, signal = yourSignal)
+amphora_api.amphorae_signals_create_signal(new_amphora.id, signal = yourSignal)
 signalStore=[]
 signalStore.append({"t": time_now, "timeProduct": time_prod})
 
 # Push signal
-amphora_api.amphorae_signals_upload_signal_batch(res.id, signalStore)
+amphora_api.amphorae_signals_upload_signal_batch(new_amphora.id, signalStore)
