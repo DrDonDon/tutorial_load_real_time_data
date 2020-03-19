ad982daac469406d5994ce7b1febad310632e156
DrDonDon <53420422+DrDonDon@users.noreply.github.com>
2020-03-19 17:05:32 +1000

    Create Amphora

--- Step_4.py
+++ Step_4.py
@@ -19,11 +19,44 @@ token_request = a10a.TokenRequest(username=os.getenv('username'), password=os.ge
 amphora_api = a10a.AmphoraeApi(a10a.ApiClient(configuration))
 
 # Define model function
+def time_product(date_time)
+  time_hour = date_time.hour
+  time_minute = date_time.minute
+  time_second = date_time.second
 
-# Define create Amphora function
+  time_prod = time_hour * time_minute * time_second
+  
+  return time_prod
 
 #####################################
 
 # Run model
+time_now = date_time.utcnow()
+time_prod = time_product(time_now)
 
 # Create Amphora
+# Define metadata for Amphora
+sep=" "
+amphora_description = "This is an Amphora for a tutorial on how to make Amphoras"
+amphora_tnc = "Creative_Commons_4p0"
+amphora_name = "Tutorial: How to make and upload an Amphora"
+amphora_labels = "tutorial,timeseries"
+amphora_price = 10   # Monthly price of Amphora
+amphora_lat = -27.45714
+amphora_lon = 153.07106
+
+## Create an Amphora 
+dto = a10a.CreateAmphora(name = amphora_name, lat = amphora_lat, lon = amphora_lon, 
+                         price = amphora_price, description = amphora_description, 
+                         terms_and_conditions_id = amphora_tnc, labels = amphora_labels)
+res = amphora_api.amphorae_create(create_amphora=dto)
+
+# Now create signal 
+yourSignal=a10a.Signal(_property = "timeProduct", value_type = "Numeric", 
+                       attributes = {"units": "HMS"})
+amphora_api.amphorae_signals_create_signal(res.id, signal = yourSignal)
+signalStore=[]
+signalStore.append({"t": time_now, "timeProduct": time_prod})
+
+# Push signal
+amphora_api.amphorae_signals_upload_signal_batch(res.id, signalStore)
